34fe2fc2c540c7ef9b4cdb3d87e32e8058bc061f
Gauthier Delmee <gauthier.delmee@cyara.com>
2019-03-05 22:04:32 +1100

    added custom 404 error handling

--- main.go
+++ main.go
@@ -7,6 +7,12 @@ import (
 )
 
 func rootHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		w.WriteHeader(http.StatusNotFound)
+		w.Write([]byte("Asset Not Found\n"))
+
+		return
+	}
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte("Running API v1\n"))
 }
