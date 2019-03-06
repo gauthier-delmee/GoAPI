4c2043df10f06e5d97f414e729880efe73794014
Gauthier Delmee <gauthier.delmee@cyara.com>
2019-03-06 11:32:26 +1100

    created the scaffold for all the API functions

--- handlers/usersRouter.go
+++ handlers/usersRouter.go
@@ -1,11 +1,47 @@
 package handlers
 
 import (
-	"fmt"
 	"net/http"
+	"strings"
+
+	"gopkg.in/mgo.v2/bson"
 )
 
 // UsersRouter handls the users route
 func UsersRouter(w http.ResponseWriter, r *http.Request) {
-	fmt.Println(r.URL.Path)
+	path := strings.TrimSuffix(r.URL.Path, "/")
+
+	if path == "/users" {
+		switch r.Method {
+		case http.MethodGet:
+			return
+		case http.MethodPost:
+			return
+		default:
+			postError(w, http.StatusMethodNotAllowed)
+			return
+		}
+	}
+
+	path = strings.TrimPrefix(path, "/users/")
+	if !bson.IsObjectIdHex(path) {
+		postError(w, http.StatusNotFound)
+		return
+	}
+
+	// id := bson.ObjectIdHex(path)
+
+	switch r.Method {
+	case http.MethodGet:
+		return
+	case http.MethodPut:
+		return
+	case http.MethodPatch:
+		return
+	case http.MethodDelete:
+		return
+	default:
+		postError(w, http.StatusMethodNotAllowed)
+		return
+	}
 }
